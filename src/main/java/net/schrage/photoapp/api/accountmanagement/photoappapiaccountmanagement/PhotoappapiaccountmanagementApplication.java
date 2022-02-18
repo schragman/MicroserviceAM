c7327f964e719b86939e90e807b9304f75005325
schragman <schragman@gmail.com>
2022-02-18 16:05:13 +0100

    Initial Commit

---
+++ src/main/java/net/schrage/photoapp/api/accountmanagement/photoappapiaccountmanagement/PhotoappapiaccountmanagementApplication.java
@@ -0,0 +1,13 @@
+package net.schrage.photoapp.api.accountmanagement.photoappapiaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappapiaccountmanagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhotoappapiaccountmanagementApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/schrage/photoapp/api/accountmanagement/photoappapiaccountmanagement/PhotoappapiaccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.photoapp.api.accountmanagement.photoappapiaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiaccountmanagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
